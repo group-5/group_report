ed06c55388a9becbb3742c21eaff41b2b17390da
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2016-02-17 23:32:20 +0000

    Success!
    implement minted successfully
    add software section with example listing

--- Code/test.ino
+++ Code/test.ino
@@ -1,9 +1,7 @@
 #include <Servo.h>
 
-
 int pos = 0;
 Servo myservo;
-
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -12,14 +10,12 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-
-    for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees in steps of 1 degree
-        myservo.write(pos);               // tell servo to go to position in variable 'pos'
-        delay(15);                        // waits 15ms for the servo to reach the position
+    for (pos = 0; pos <= 180; pos += 1) { // go from 0-180 deg in steps of 1 deg
+        myservo.write(pos);               // tell servo to go to position 'pos'
+        delay(15);                        // wait 15ms for servo to reach position
     }
-    for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-        myservo.write(pos);               // tell servo to go to position in variable 'pos'
-        delay(15);                        // waits 15ms for the servo to reach the position
+    for (pos = 180; pos >= 0; pos -= 1) { // go from 180-0 deg
+        myservo.write(pos);               // tell servo to go to position 'pos'
+        delay(15);                        // wait 15ms for servo to reach position
     }
-
 }
